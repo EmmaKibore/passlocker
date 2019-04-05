7f9ba5f5091f8311a37f0a9a27a678cef0cf47f0
EmmaKibore <emmakibore@gmail.com>
2019-04-06 02:00:05 +0300

    added 4 modules

---
+++ user_test.py
@@ -0,0 +1,17 @@
+import unittest 
+from user import User # importing the user class
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+    unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+
+def setUp(self):
+    '''
+    set up method to run before each test cases.
+    '''
+        
