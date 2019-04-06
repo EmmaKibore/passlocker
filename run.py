c06d69be154d1c341877d97f7f52fe3f14f00a34
EmmaKibore <emmakibore@gmail.com>
2019-04-06 17:34:03 +0300

    added input

--- run.py
+++ run.py
@@ -6,4 +6,26 @@ def main():
       print('\n')
 
       while True:
-          print("Use these short codes : cc -  create a new account, dc - display accounts, fc")
+          print("Use these short codes : cc -  create a new account, dc - display accounts, fc - find an account, ex - exit the accounts")
+
+          short_code = input().lower()
+
+          if short_code == 'ca':
+              print("New account")
+              print()
+
+              print(user_name.......")
+
+              save_accounts(create_account(account ,user_name,password))
+              print('\n')
+              print(f"New Account {user_name} created")
+              print ('\n')
+
+           elif short_code == 'da':
+
+               if display_accounts():
+                   print("Here is a list of all your accounts")
+                   print('\n')
+
+                   for account in display_accounts():
+                       print(f"{account.account} {account})    
