33859a809f7562d5519784aea242a52d3fb50b4c
EmmaKibore <emmakibore@gmail.com>
2019-04-06 18:15:52 +0300

    added search method

--- run.py
+++ run.py
@@ -36,24 +36,24 @@ def main():
                   print("You dont seem to have any accounts"
                   print('\n')
 
-        #  elif short_code == 'fa':
-        #      print("Enter the account you want to search for")
+         elif short_code == 'fa':
+             print("Enter the account you want to search for")
 
-        #      search_account = input()
-        #      if check_existing_accounts(search_account):
-        #          search_account = find_account()
-        #          print(f"{search_account.user_name} {search_account.password}")
-        #          print('-' * 20)
+             search_account = input()
+             if check_existing_accounts(search_account):
+                 search_account = find_account()
+                 print(f"{search_account.user_name} {search_account.password}")
+                 print('-' * 20)
 
-        #          print(f"Username.........{search_account.user_name}")
-        #          print(f"Password..........{search_account.password}")
-        #      else:
-        #          print("That contact does not exist")
+                 print(f"Username.........{search_account.user_name}")
+                 print(f"Password..........{search_account.password}")
+             else:
+                 print("That contact does not exist")
 
-        #  elif short_code == "ex":
-        #         print("Bye ...........")
-        #         break
-        #      else:
-        #          print("I did not get that at all. Please use the short codes")                                 
+         elif short_code == "ex":
+                print("Bye ...........")
+                break
+             else:
+                 print("I did not get that at all. Please use the short codes")                                 
 
 
