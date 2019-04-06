fda831ef54d12ca9f6e97f132603218de557752e
EmmaKibore <emmakibore@gmail.com>
2019-04-06 05:27:02 +0300

    added description in README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+#Password Locker
+This is a n application that allows us to generate and store passwords for various accounts.
--- user_test.py
+++ user_test.py
@@ -14,3 +14,13 @@ def setUp(self):
     '''
     set up method to run before each test cases.
     '''
+    self.new_user = User("Instagram", "Emma", "0000") # create user object
+
+def test_init(self):
+     '''
+     test_init case to test if the object is initialized correctly
+     '''
+
+     self.assertEqual(self.new_user.account,"Instagram")
+     self.assertEqual(self.new_user.username,"Emma")
+     self.assertEqual(self.new_user.password,"0000")
