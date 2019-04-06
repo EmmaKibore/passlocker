0f603b5975aab44fd7988ab942c73278d1ea95ff
EmmaKibore <emmakibore@gmail.com>
2019-04-06 18:09:46 +0300

    added user_name input

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ def main():
       print('\n')
 
       while True:
-          print("Use these short codes : cc -  create a new account, dc - display accounts, fc - find an account, ex - exit the accounts")
+          print("Use these short codes : ca -  create a new account, da - display accounts, fa - find an account, ex - exit the accounts")
 
           short_code = input().lower()
 
@@ -21,11 +21,39 @@ def main():
               print(f"New Account {user_name} created")
               print ('\n')
 
-           elif short_code == 'da':
+        #  elif short_code == 'da':
 
-               if display_accounts():
-                   print("Here is a list of all your accounts")
-                   print('\n')
+        #        if display_accounts():
+        #            print("Here is a list of all your accounts")
+        #            print('\n')
 
-                   for account in display_accounts():
-                       print(f"{account.account} {account})    
+        #            for account in display_accounts():
+        #                print(f"{account.username}......{account.password}")
+
+        #                print('\n')
+        #  else:
+        #           print('\n')
+        #           print("You dont seem to have any accounts"
+        #           print('\n')
+
+        #  elif short_code == 'fa':
+        #      print("Enter the account you want to search for")
+
+        #      search_account = input()
+        #      if check_existing_accounts(search_account):
+        #          search_account = find_account()
+        #          print(f"{search_account.user_name} {search_account.password}")
+        #          print('-' * 20)
+
+        #          print(f"Username.........{search_account.user_name}")
+        #          print(f"Password..........{search_account.password}")
+        #      else:
+        #          print("That contact does not exist")
+
+        #  elif short_code == "ex":
+        #         print("Bye ...........")
+        #         break
+        #      else:
+        #          print("I did not get that at all. Please use the short codes")                                 
+
+
